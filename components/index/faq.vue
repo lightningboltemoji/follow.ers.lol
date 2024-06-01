ff875adc904597f54d8fff5fc21faf49e31ebfaf
Tanner <51724788+lightningboltemoji@users.noreply.github.com>
2024-05-31 23:29:46 -0700

    Change some of the copy

--- components/index/faq.vue
+++ components/index/faq.vue
@@ -15,18 +15,17 @@
     <div>
       <ul class="mt-3 pl-4 list-disc">
         <li>
-          Before selecting your export file, you can turn on <b>Airplane Mode ✈</b> to ensure your data isn't being
-          uploaded.
+          When it's time to share your data, you can turn on <b>Airplane Mode ✈</b> to ensure your data isn't leaving
+          your phone.
         </li>
         <li>
           This project is open source, so you can
-          <ext-link href="https://github.com/lightningboltemoji/follow.ers.lol">read the code</ext-link>
-          if that's your thing!
+          <ext-link href="https://github.com/lightningboltemoji/follow.ers.lol">read the code</ext-link>!
         </li>
         <li>
-          This tool was built by
-          <ext-link href="https://tanner.cecchetti.xyz">me</ext-link>. You don't really have any way to verify this, but
-          I'm just a (relatively) normal person that made this for fun. There's no secret agenda.
+          You don't have any way to verify this, but
+          <ext-link href="https://tanner.cecchetti.xyz">I'm</ext-link> just a (relatively) normal person that built this
+          for fun. There's no secret agenda.
         </li>
       </ul>
     </div>
